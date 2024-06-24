72fcfe1163cb0e94adc69a8e754edf7503146e01
keshav00001 <keshavking111@gmail.com>
2024-06-24 19:20:56 +0530

    new pages added

---
+++ src/components/Secondfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Secondfile(props) {
+  return (
+    <div>
+      Secondfile
+      <img src="./apple-touch.png" className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Secondfile;
